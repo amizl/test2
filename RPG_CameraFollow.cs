eea145c027bf19c4b98698e2b010ed3371831271
amizl <83875270+amizl@users.noreply.github.com>
2021-05-08 13:01:22 -0400

    Update RPG_CameraFollow.cs
    after simple refactoring.

--- RPG_CameraFollow.cs
+++ RPG_CameraFollow.cs
@@ -7,16 +7,9 @@ namespace RPG.Core
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] Transform target; // orignal script 
-
-       
-        private void Start()
-        {
-           
-        }
         void LateUpdate()
         {
             transform.position = target.position; // original script
-      
         }
     }
 }
